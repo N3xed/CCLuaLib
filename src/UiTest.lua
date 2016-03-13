3d9e3c087ccbcb8a6337c84ddb6ea3568b241baf
N3xed <dominik.gschwind99@gmail.cokm>
2016-03-13 18:45:08 +0100

    - Changes in UiTest.lua

--- src/UiTest.lua
+++ src/UiTest.lua
@@ -2,14 +2,17 @@ shell.run("Core.api")
 shell.run("Graphics.api")
 shell.run("Ui.api")
 
-UiManager:init()
 
-local window = new(UiWindow, 5, 5, 30, 30)
-window.title = "a window"
-local button = new(UiButton, 1, 1, 10, 5)
-button.text = "click"
-window.container:addUiObject(button)
+function init()
+  UiManager:init()
+  window = new(UiWindow, 5, 5, 30, 30)
+  window.title = "a window"
+  button = new(UiButton, 1, 1, 10, 5)
+  button.text = "click"
+  window.container:addUiObject(button)
+  
+  UiManager:addUiObject(window)
+end
 
-UiManager:addUiObject(window)
-
+init()
 EventManager:start()
